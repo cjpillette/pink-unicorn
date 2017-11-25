25b4bb8cfe4b9ec563283f77238f19ab2826abf7
Carole <cjpillette@hotmail.com>
2017-11-25 14:39:42 +1100

    created AppBar with MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import MyAppBar from './Components/Header/MyAppBar'
+
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Testing with yarn</p>
+        <MuiThemeProvider>
+          <MyAppBar />
+          <p>Testing with yarn</p>
+        </MuiThemeProvider>
       </div>
     );
   }
